4bca5743114278560b8990bfa7d234c437d5ac84
PC-16 <it.pgee.burgas@gmail.com>
2025-02-24 12:58:50 +0200

    ModelsUpdate added a ToDoList.cs

---
+++ Models/ToDoList.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoApp.Models
+{
+    public class ToDoList
+    {
+        public int Id { get; set; }
+        public string ToDo { get; set; }
+        public string Chores { get; set; }
+    }
+}
