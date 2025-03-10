71fa38d486ca5a337b74c765c42b6aedb3ab9d5e
PC15 <zlati.2006a@gmail.com>
2025-03-10 12:11:27 +0200

    Added CRUD operations

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ToDoApp.Models;
 
 namespace ToDoApp.Data
 {
@@ -9,5 +10,6 @@ namespace ToDoApp.Data
             : base(options)
         {
         }
+        public DbSet<ToDoItem> ToDoItems { get; set; }
     }
 }
